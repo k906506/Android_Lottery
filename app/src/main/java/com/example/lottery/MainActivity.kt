3ee8cc8bdd0dd5b6b1068241e10beb43579813e9
k906506 <k906506@naver.com>
2021-05-12 11:56:45 +0900

    번호 자동생성, 번호 직접추가 구현

--- app/src/main/java/com/example/lottery/MainActivity.kt
+++ app/src/main/java/com/example/lottery/MainActivity.kt
@@ -2,10 +2,96 @@ package com.example.lottery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import org.w3c.dom.Text
+import java.util.HashSet
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton: Button by lazy{
+        findViewById<Button>(R.id.clearButton)
+    }
+
+    private val addButton : Button by lazy{
+        findViewById<Button>(R.id.addButton)
+    }
+
+    private val autoMakeButton : Button by lazy{
+        findViewById<Button>(R.id.autoMake)
+    }
+
+    private val numberPicker: NumberPicker by lazy{
+        findViewById<NumberPicker>(R.id.numberPicker)
+    }
+
+    private val numberTextViewList: List<TextView> by lazy{
+        listOf<TextView>(
+            findViewById<TextView>(R.id.firstNumber),
+            findViewById<TextView>(R.id.secondNumber),
+            findViewById<TextView>(R.id.thirdNumber),
+            findViewById<TextView>(R.id.fourthNumber),
+            findViewById<TextView>(R.id.fifthNumber),
+            findViewById<TextView>(R.id.sixthNumber)
+        )
+    }
+
+    private var didRun = false
+    private val pickNumberSet = hashSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        initAutoMakeButton()
+        initAddButton()
     }
+
+    private fun initAutoMakeButton(){
+        autoMakeButton.setOnClickListener(){
+            val list = getRandomNumber()
+            Log.d("hi", list.toString())
+        }
+    }
+
+    private fun initAddButton(){
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요. ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.size >= 6) {
+                Toast.makeText(this, "번호는 6개까지만 추가할 수 있습니다. ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pickNumberSet.contains(numberPicker.value)) {
+                Toast.makeText(this, "이미 선택된 번호입니다. ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val textView = numberTextViewList[pickNumberSet.size]
+            textView.isVisible = true
+            textView.text = numberPicker.value.toString()
+            pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun getRandomNumber(): List<Int>{
+        val numberList = mutableListOf<Int>().apply {
+            for (i in 1..45) {
+                this.add(i)
+            }
+        }
+        numberList.shuffle()
+        return numberList.subList(0, 6).sorted()
+    }
+
 }
